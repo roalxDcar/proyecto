e28b60e78a73b61331ea27d8571041521e50e988
roalxDcar <roalmollericona@gmail.com>
2019-05-31 22:59:18 -0400

    nuevo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You  in!
                 </div>
             </div>
         </div>
