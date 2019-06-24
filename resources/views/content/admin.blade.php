3a0dc90411c8052c62d278fff426b5712f9a3758
HayarAlex <hayaralexaliagagutierrez@gmail.com>
2019-06-24 00:27:12 -0400

    new imagen

--- resources/views/content/admin.blade.php
+++ resources/views/content/admin.blade.php
@@ -5,8 +5,9 @@
           <div class="absolute">
             <h1>Esto es INFO-SPORT!!</h1>
           </div>
-          <video onloadedmetadata="this.muted=true" autoplay loop>
-            <source src="../assets/images/we.mp4">
-          </video>
+          {{-- <video onloadedmetadata="this.muted=true" autoplay loop>
+            <source src="../assets/images/final.jpg">
+          </video> --}}
+          <img src="../assets/images/final.jpg" alt="">
         </header>
 @endsection
